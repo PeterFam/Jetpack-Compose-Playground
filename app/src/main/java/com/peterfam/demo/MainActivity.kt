2c67cb0c1decb3b7b0f8a2ee64e05c1ee9e38dc4
peterfam <peter.fam@advansys-esc.com>
2022-11-16 13:19:46 +0200

    trying compose state

--- app/src/main/java/com/peterfam/demo/MainActivity.kt
+++ app/src/main/java/com/peterfam/demo/MainActivity.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.peterfam.demo.ui.components.ColorBox
 import com.peterfam.demo.ui.components.CustomText
 import com.peterfam.demo.ui.components.ImageCard
 
@@ -20,13 +23,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier
-                .fillMaxSize()
-                .background(Color.DarkGray),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center) {
-                CustomText()
-                Text(text = "Custom Text with annotated String", color = Color.White)
+
+            val color = remember {
+                mutableStateOf(Color.Yellow)
+            }
+            Column(Modifier.fillMaxSize()) {
+                ColorBox(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ) {
+                    color.value = it
+                }
+                Box(modifier = Modifier
+                    .background(color = color.value)
+                    .weight(1f)
+                    .fillMaxSize())
             }
         }
     }
@@ -35,10 +47,12 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun PreviewImageCard(){
-        ImageCard(painter = painterResource(id = R.drawable.test_ims),
-            contendDescription = "",
-            title = "This is a test text to test text vision")
+fun PreviewImageCard() {
+    ImageCard(
+        painter = painterResource(id = R.drawable.test_ims),
+        contendDescription = "",
+        title = "This is a test text to test text vision"
+    )
 }
 
 
---
+++ app/src/main/java/com/peterfam/demo/ui/components/ColorBox.kt
@@ -0,0 +1,29 @@
+package com.peterfam.demo.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import kotlin.random.Random
+
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updateColor: (Color) -> Unit
+){
+    Box(modifier = modifier.background(
+        Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            )
+        }
+    )
+}
