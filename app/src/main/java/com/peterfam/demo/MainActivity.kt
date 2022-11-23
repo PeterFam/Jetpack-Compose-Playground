e7b90236d1a8e4c066bfbc18763347972681fa84
peterfam <peter.fam@advansys-esc.com>
2022-11-23 12:03:12 +0200

    apply compose lists using lazyColumn

--- app/src/main/java/com/peterfam/demo/MainActivity.kt
+++ app/src/main/java/com/peterfam/demo/MainActivity.kt
@@ -4,50 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.peterfam.demo.ui.model.User
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scaffoldState = rememberScaffoldState()
-            var textFieldState by remember {
-                mutableStateOf("")
-            }
+            LazyColumn {
+                itemsIndexed(
+                    listOf(
+                        User("Mohamed", "Salah", 29),
+                        User("Leonel", " Messi", 35),
+                        User("Christiano", "Ronaldo", 36)
+                    )
+                ){ index, user ->
+                    Text(modifier = Modifier.fillMaxWidth().padding(vertical = 24.dp),
 
-            val scope = rememberCoroutineScope()
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-                scaffoldState = scaffoldState
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 30.dp)
-                ) {
-                    OutlinedTextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        singleLine = true,
-                        value = textFieldState,
-                        label = { Text(text = "Enter your name:") },
-                        onValueChange = {
-                            textFieldState = it
-                        })
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(onClick = {
-                        scope.launch {
-                            scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
-                        }
-                    }) {
-                        Text(text = "Greet me!")
-                    }
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 30.sp,
+                        text = "${user.firstName} ${user.lastName} with age ${user.age}")
                 }
             }
         }
---
+++ app/src/main/java/com/peterfam/demo/ui/model/User.kt
@@ -0,0 +1,7 @@
+package com.peterfam.demo.ui.model
+
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val age: Int
+)
